824c140a4584ed102ce49b3821379da08ed887d3
Melker <melkerrydman@gmail.com>
2022-10-10 09:29:56 +0200

    Basic input and styling changes to Search component

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -1,8 +1,23 @@
-import { Paragraph } from '../Typography/Typography';
+import { useState } from 'react';
 
 import styles from './Search.module.scss';
 
 const Search = () => {
-  return <Paragraph as="span">Search for artist, song or album</Paragraph>;
+  const [searchValue, setSearchValue] = useState<string>('');
+  const [searchResult, setSearchResult] = useState([]);
+
+  return (
+    <>
+      <input
+        className={styles['search_input']}
+        id="search"
+        name="search"
+        type="text"
+        placeholder="Search for artist, song or album"
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+      />
+    </>
+  );
 };
 export default Search;
