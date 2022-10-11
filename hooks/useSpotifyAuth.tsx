bbce63aa3262b3b4f4ee9b93f82eba393cfe893c
melkerydman <melkerrydman@gmail.com>
2022-10-11 19:03:48 +0200

    Created authentication hook

--- hooks/useSpotifyAuth.tsx
+++ hooks/useSpotifyAuth.tsx
@@ -1,1 +1,10 @@
-export const useSpotifyAuth = () => {};
+import axios from 'axios';
+
+const url = 'http://localhost:3000/api/auth';
+
+export const useSpotifyAuth = () => {
+  return axios
+    .get(url)
+    .then((res) => res.data.body.access_token)
+    .catch((error) => error);
+};
