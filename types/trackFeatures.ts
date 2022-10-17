9b99492c1e2b658cd376cbbc713f90c6b39c59bb
melkerydman <melkerrydman@gmail.com>
2022-10-17 17:43:47 +0200

    Initial TrackFeatures types and function

---
+++ types/trackFeatures.ts
@@ -0,0 +1,20 @@
+export interface TrackFeatures {
+  acousticness: number;
+  analysisURL: string;
+  danceability: number;
+  durationMS: number;
+  energy: number;
+  id: string;
+  instrumentalness: number;
+  key: number;
+  liveness: number;
+  loudness: number;
+  mode: number;
+  speechiness: number;
+  tempo: number;
+  timeSignature: number;
+  trackHref: string;
+  type: string;
+  uri: string;
+  valence: number;
+}
