13c71b994750f3c5f0c06d7c58b77598d28250a2
Melker <melkerrydman@gmail.com>
2022-10-18 14:42:10 +0200

    Simple render of lyrics

--- pages/track/[...trackId].tsx
+++ pages/track/[...trackId].tsx
@@ -1,18 +1,29 @@
-import { useSpotifyAuth } from '../../hooks/useSpotifyAuth';
-import getTrack from '../../services/spotify/getTrack';
+import { useSpotifyAuth } from '../../utilities/hooks/useSpotifyAuth';
+import getTrack from '../../utilities/services/spotify/getTrack';
 
-import { Heading } from '../../components/Typography/Typography';
-import getTrackFeatures from '../../services/spotify/getTrackFeatures';
-import getLyrics from '../../services/musixmatch/getLyrics';
+import { Heading, Paragraph } from '../../components/Typography/Typography';
+import getTrackFeatures from '../../utilities/services/spotify/getTrackFeatures';
+import getLyrics from '../../utilities/services/musixmatch/getLyrics';
+import Header from '../../components/Header/Header';
 
+// TODO: Remove "styles" and create a layout component for TrackPage instead
+import styles from './track.module.scss';
+
+// TODO: Type props
 const Track = ({ trackData, trackFeatures, lyrics }) => {
   const artists = trackData.artists.map((artist) => artist.name);
 
   return (
-    <div>
-      <Heading as="h1">{trackData.name}</Heading>;
-      <Heading as="h3">{artists.join(', ')}</Heading>;
-    </div>
+    <>
+      <Header />
+      <div>
+        <Heading as="h1">{trackData.name}</Heading>;
+        <Heading as="h3">{artists.join(', ')}</Heading>;
+        <Paragraph className={styles.lyrics} as="span">
+          {lyrics.lyrics_body}
+        </Paragraph>
+      </div>
+    </>
   );
 };
 
