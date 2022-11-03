34126e798223a62e1b0b445cb4d2a87bbff0dd72
Melker <melkerrydman@gmail.com>
2022-11-03 19:10:30 +0100

    Handle content positioning, overflows and container heights, most likely not perfect

--- components/pages/TrackPage/TrackPage.tsx
+++ components/pages/TrackPage/TrackPage.tsx
@@ -1,4 +1,5 @@
 import { Album, Lyrics, Track, TrackFeatures } from '../../../types';
+import { handleClassName } from '../../../utilities/helpers';
 import { Display, Heading, Paragraph } from '../../Typography/Typography';
 import AlbumAndFeatures from './AlbumAndFeatures/AlbumAndFeatures';
 
@@ -15,24 +16,25 @@ type Props = {
   data: Data;
 };
 
+// TODO: Proper class names
 const TrackPage = ({ data }: Props) => {
   const { album, track, features, lyrics } = data;
 
   const artists = track.artists.map((artist) => artist.name);
 
   return (
-    <main className="container">
-      <header>
+    <main className={handleClassName(['container', styles['page']])}>
+      <header className={styles['header']}>
         <Heading as="h3">{track.type}</Heading>
         <Display as="h1" small>
           {track.name}
         </Display>
         <Heading as="h2">{artists.join(', ')}</Heading>
       </header>
-      {/* //TODO: Remove inline styling   */}
-      <section style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <section className={styles['content']}>
         {/* //TODO: Create lyrics component */}
-        <div>
+        <div className={styles['lyrics-outer']}>
+          <Heading as="h4">Lyrics</Heading>
           <Paragraph className={styles.lyrics} as="span">
             {lyrics.lyrics_body}
           </Paragraph>
