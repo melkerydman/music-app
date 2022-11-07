bfafc58f962371d316589e09f3ac9449e28bcd1b
melkerydman <melkerrydman@gmail.com>
2022-11-07 07:38:13 +0100

    Fixed more linting errors

---
+++ components/pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,6 @@
+// eslint-disable-next-line arrow-body-style
+const LandingPage = () => {
+  return <div>LandingPage</div>;
+};
+
+export default LandingPage;
---
+++ components/pages/SearchResultsPage/SearchResultsPage.tsx
@@ -0,0 +1,6 @@
+// eslint-disable-next-line arrow-body-style
+const SearchResultsPage = () => {
+  return <div>SearchResultsPage</div>;
+};
+
+export default SearchResultsPage;
