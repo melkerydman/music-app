4215620868517266c8d2c13da0a10526640751bd
melkerydman <melkerrydman@gmail.com>
2022-11-10 15:36:48 +0100

    Trigger rebuild

--- utilities/hooks/useSpotifyAuth.tsx
+++ utilities/hooks/useSpotifyAuth.tsx
@@ -3,13 +3,10 @@ import axios from 'axios';
 // TODO: Refactor useSpotifyAuth to be the main function, with getting and setting token cookies within that one instead?
 // TODO: Replace with .env url
 const dev = process.env.NEXT_PUBLIC_NODE_ENV !== 'production';
-const baseUrl = dev
-  ? 'http://localhost:3000'
-  : process.env.NEXT_PUBLIC_PAGE_URL;
 
 const useSpotifyAuth = () =>
   axios
-    .get(`${baseUrl}/api/auth`)
+    .get(`${dev ? 'http://localhost:3000' : ''}/api/auth`)
     .then((res) => res.data.body)
     .catch((error) => error);
 
