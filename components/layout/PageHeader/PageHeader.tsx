183f1bb41f11b1d499dd6b4e5defd8a8ead2ac8d
Melker <melkerrydman@gmail.com>
2022-12-21 13:48:25 +0100

    PageHeader updates to work on different pages

---
+++ components/layout/PageHeader/PageHeader.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/future/image';
+import { Heading } from '../../Typography/Typography';
+import styles from './PageHeader.module.scss';
+
+interface Props {
+  image: SpotifyApi.ImageObject;
+  heading: string;
+  subHeading: string;
+}
+
+const PageHeader = ({ image, heading, subHeading }: Props) => {
+  const { url: imageUrl } = image;
+
+  return (
+    <header className={styles.header}>
+      <div className={styles['image-wrapper']}>
+        <Image
+          src={imageUrl}
+          alt={heading}
+          className={styles.image}
+          sizes="100vw"
+          fill
+        />
+      </div>
+      <div className={styles.titles}>
+        <Heading as="h2">{subHeading}</Heading>
+        <Heading as="h1">{heading}</Heading>
+      </div>
+    </header>
+  );
+};
+
+export default PageHeader;
