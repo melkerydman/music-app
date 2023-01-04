bcf26c622358b28bcfe5dbd262d2f3a8e71bab84
Melker <melkerrydman@gmail.com>
2023-01-04 17:01:07 +0100

    Reworked the SearchInput component

--- components/Search/SearchBox/SearchBox.tsx
+++ components/Search/SearchBox/SearchBox.tsx
@@ -1,9 +1,17 @@
 import React, { useEffect, useRef } from 'react';
 import useStore from '../../../store/useStore';
+import { handleClassName } from '../../../utilities/helpers';
 
-import styles from './SearchInput.module.scss';
+import styles from './SearchBox.module.scss';
 
-const SearchInput = () => {
+type Props = {
+  active?: boolean;
+  className?: string;
+};
+
+const SearchBox = ({ active, className, ...rest }: Props) => {
+  const inputRef = useRef(null);
+
   const search = useStore((state) => state.search.search);
   const setSearch = useStore((state) => state.search.setSearch);
   const isFocus = useStore((state) => state.search.isFocus);
@@ -23,24 +31,38 @@ const SearchInput = () => {
     return () => document.body.removeEventListener('click', closeSearch);
   }, [setIsFocus]);
 
+  useEffect(() => {
+    if (active) {
+      inputRef?.current?.focus();
+    }
+  }, [active]);
+
   return (
-    <>
+    <div className={handleClassName([styles.wrapper, className && className])}>
+      <button>icon</button>
       <input
-        className={styles.search_input}
+        className={handleClassName([styles.input])}
         id="search"
+        ref={inputRef}
         name="search"
         type="text"
         placeholder="Search for artist, song or album"
         value={search}
         onChange={(e) => setSearch(e.target.value)}
         onFocus={() => setIsFocus(true)}
+        {...rest}
       />
-      {/* // TODO: is search !== "" needed below? */}
-      {search !== '' && isFocus && (
-        <div ref={overlayRef} className={styles.overlay} />
-      )}
-    </>
+      <button
+        className={handleClassName([!isFocus && 'hidden'])}
+        onClick={() => {
+          setIsFocus(false);
+        }}
+      >
+        X
+      </button>
+    </div>
   );
 };
+// {isFocus && <div ref={overlayRef} className={styles.overlay} />}
 
-export default SearchInput;
+export default SearchBox;
