ffe1ab12a0d4ecf097f9bda666989faae0e45aef
Melker <melkerrydman@gmail.com>
2023-01-05 17:18:00 +0100

    ..

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import useStore from '../../store/useStore';
 import { handleClassName } from '../../utilities/helpers';
 import Logo from '../Logo/Logo';
@@ -14,15 +14,59 @@ type Props = {
 
 const Header = ({ className, containerClassName }: Props): JSX.Element => {
   const [isMobile, setIsMobile] = useState(false);
+  const [keyboardActive, setKeyboardActive] = useState(false);
   const isFocus = useStore((state) => state.search.isFocus);
+  // TODO: Remove from here...
+  const headerRef = useRef<HTMLDivElement>(null);
+  const originalY = useRef(0);
+
+  const handleFocus = () => {
+    console.log('handleFocus() 🔴');
+    if (headerRef.current) {
+      originalY.current = window.pageYOffset;
+      const inputRect = headerRef.current.getBoundingClientRect();
+      window.scrollTo(0, inputRect.top + window.pageYOffset);
+    }
+  };
+
+  const handleResize = () => {
+    console.log('handleResize() 🔴');
+    if (originalY.current !== 0) {
+      window.scrollTo(0, originalY.current);
+      originalY.current = 0;
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  // TODO: ... to here if doesn't fix viewport height issue on mobile when keyboard opens. As well as handleFocus in onFocus in input element
 
   useEffect(() => {
     setIsMobile(window.innerWidth < 768);
   }, []);
 
+  useEffect(() => {
+    if (isMobile && isFocus) {
+      setKeyboardActive(true);
+    } else {
+      setKeyboardActive(false);
+    }
+  }, [isMobile, isFocus]);
+
+  useEffect(() => {
+    if (keyboardActive) {
+      handleFocus();
+    }
+  }, [keyboardActive]);
+
   // TODO: Maybe hide the Logo and Metronome visually instead of not rendering them?
   return (
     <header
+      ref={headerRef}
       className={handleClassName([
         styles.header,
         className || '',
