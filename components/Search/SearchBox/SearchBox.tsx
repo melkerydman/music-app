1bbe30271f55eb37cdd529940e468c1121fa8ee3
Melker <melkerrydman@gmail.com>
2023-01-05 13:57:48 +0100

    Style fixes

--- components/Search/SearchBox/SearchBox.tsx
+++ components/Search/SearchBox/SearchBox.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import useStore from '../../../store/useStore';
 import { handleClassName } from '../../../utilities/helpers';
 
@@ -11,25 +11,16 @@ type Props = {
 
 const SearchBox = ({ active, className, ...rest }: Props) => {
   const inputRef = useRef(null);
+  const [isMobile, setIsMobile] = useState(false);
 
   const search = useStore((state) => state.search.search);
   const setSearch = useStore((state) => state.search.setSearch);
   const isFocus = useStore((state) => state.search.isFocus);
   const setIsFocus = useStore((state) => state.search.setIsFocus);
 
-  // TODO: Create reusable hook for adding overlay?
-  // TODO: Add the eventlistener to the ref instead of the entire body
-  const overlayRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
-    const closeSearch = (e) => {
-      if (e.target === overlayRef.current) {
-        setIsFocus(false);
-      }
-    };
-    document.body.addEventListener('click', closeSearch);
-
-    return () => document.body.removeEventListener('click', closeSearch);
-  }, [setIsFocus]);
+    setIsMobile(window.innerWidth < 768);
+  }, []);
 
   useEffect(() => {
     if (active) {
@@ -39,9 +30,12 @@ const SearchBox = ({ active, className, ...rest }: Props) => {
 
   return (
     <div className={handleClassName([styles.wrapper, className && className])}>
-      <button>icon</button>
+      <button onClick={() => setIsFocus(true)}>🔦</button>
       <input
-        className={handleClassName([styles.input])}
+        className={handleClassName([
+          styles.input,
+          isMobile && !isFocus && 'hidden',
+        ])}
         id="search"
         ref={inputRef}
         name="search"
@@ -63,6 +57,5 @@ const SearchBox = ({ active, className, ...rest }: Props) => {
     </div>
   );
 };
-// {isFocus && <div ref={overlayRef} className={styles.overlay} />}
 
 export default SearchBox;
