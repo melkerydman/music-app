8a6898d9aa3dda89cc9092d126bfd246424d913f
Melker <melkerrydman@gmail.com>
2023-01-23 17:06:51 +0100

    Initial useHover hook - needs work

--- utilities/hooks/index.ts
+++ utilities/hooks/index.ts
@@ -2,10 +2,12 @@ import useAuthToken from './useAuthToken';
 import useWindowDimensions from './useWindowDimensions';
 import useOnScreenKeyboardScrollFix from './useOnScreenKeyboardScrollFix';
 import useMetronome from './useMetronome';
+import useHover from './useHover';
 
 export {
   useAuthToken,
   useWindowDimensions,
   useOnScreenKeyboardScrollFix,
   useMetronome,
+  useHover,
 };
