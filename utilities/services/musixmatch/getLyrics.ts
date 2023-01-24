fee2a01f93a3c40aad53d4c9490230d8100ab236
Melker <melkerrydman@gmail.com>
2023-01-24 16:07:51 +0100

    Reworked lyrics fetching to work on client

--- utilities/services/musixmatch/getLyrics.ts
+++ utilities/services/musixmatch/getLyrics.ts
@@ -1,33 +1,13 @@
 import axios from 'axios';
 
-const apiKey = process.env.MUSIXMATCH_API_KEY;
-const baseUrl = process.env.MUSIXMATCH_BASE_URL;
+const dev = process.env.NEXT_PUBLIC_NODE_ENV !== 'production';
 
-// TODO: Add @params description
-const getMusixmatchId = async (isrc: string) => {
-  const url = `${baseUrl}/track.get?apikey=${apiKey}&track_isrc=${isrc}`;
-
-  return axios
-    .get(url)
-    .then((response) => response.data.message.body.track.track_id)
+const getLyrics = (isrc) =>
+  axios
+    .get(`${dev ? 'http://localhost:3000' : ''}/api/lyrics/${isrc}`)
+    .then((res) => res.data.lyrics)
     .catch((error) => {
-      console.log(error);
-      return error;
+      throw new Error(`Failed to get lyrics: ${error.message}`);
     });
-};
-
-// TODO: Add @params description
-const getLyrics = async (isrc: string) => {
-  const musixmatchId = await getMusixmatchId(isrc);
-  const url = `${baseUrl}/track.lyrics.get?apikey=${apiKey}&track_id=${musixmatchId}`;
-
-  return axios
-    .get(url)
-    .then((response) => response.data.message.body.lyrics)
-    .catch((error) => {
-      console.log('GetLyrics Error: ', error);
-      return error;
-    });
-};
 
 export default getLyrics;
