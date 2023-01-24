490cb23199f6e6f15a904011ae51ef591e79ef0c
Melker <melkerrydman@gmail.com>
2023-01-24 16:57:45 +0100

    Initial incorporation of lyrics fetching on client

--- components/pages/TrackPage/Lyrics/Lyrics.tsx
+++ components/pages/TrackPage/Lyrics/Lyrics.tsx
@@ -1,26 +1,58 @@
+import { useEffect, useState } from 'react';
+import getLyrics from '../../../../utilities/services/musixmatch/getLyrics';
 import { Lyrics as LyricsType } from '../../../../types';
 import { Heading, Paragraph } from '../../../Typography/Typography';
+
 import styles from './Lyrics.module.scss';
 
-type Data = {
-  lyrics: LyricsType;
-};
-
 type Props = {
-  data: Data;
+  isrc: string;
 };
 
-const Lyrics = ({ data }: Props): JSX.Element => {
-  const { lyrics } = data;
+function splitText(text) {
+  return text.split(/\n{2,}|\n/);
+}
 
+const Lyrics = ({ isrc }: Props): JSX.Element => {
+  const [fetchedLyrics, setFetchedLyrics] = useState<any[]>(null);
+
+  useEffect(() => {
+    const fetchLyrics = async () => {
+      try {
+        const result = (await getLyrics(isrc)) as LyricsType;
+        const lyrics = splitText(result.lyrics_body);
+        setFetchedLyrics(lyrics);
+      } catch {
+        setFetchedLyrics(['Lyrics not found.']);
+      }
+    };
+    fetchLyrics();
+  }, [isrc]);
+
+  const DisplayLyrics = () => {
+    if (fetchedLyrics?.length > 1) {
+      return (
+        <div className={styles.lyrics}>
+          {fetchedLyrics.map((lyric, index) => (
+            <Paragraph key={index} as="div">
+              {lyric}
+            </Paragraph>
+          ))}
+        </div>
+      );
+    }
+    return (
+      <Paragraph sans className={styles.lyrics} as="div">
+        {fetchedLyrics}
+      </Paragraph>
+    );
+  };
   return (
     <div className={styles.lyrics__outer}>
       <Heading className={styles.lyrics__heading} as="h3">
         Lyrics
       </Heading>
-      <Paragraph sans className={styles.lyrics} as="span">
-        {lyrics ? lyrics.lyrics_body : 'Lyrics not found 😤'}
-      </Paragraph>
+      <DisplayLyrics />
     </div>
   );
 };
--- utilities/services/musixmatch/getLyrics.ts
+++ utilities/services/musixmatch/getLyrics.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const dev = process.env.NEXT_PUBLIC_NODE_ENV !== 'production';
 
-const getLyrics = (isrc) =>
+const getLyrics = (isrc: string) =>
   axios
     .get(`${dev ? 'http://localhost:3000' : ''}/api/lyrics/${isrc}`)
     .then((res) => res.data.lyrics)
