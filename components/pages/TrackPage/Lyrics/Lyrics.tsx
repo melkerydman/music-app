eaf33840331d85ac5bfc787a199deb6938b6b6d5
Melker <melkerrydman@gmail.com>
2023-01-26 09:28:45 +0100

    Refactor and added option to render two columns

--- components/pages/TrackPage/Lyrics/Lyrics.tsx
+++ components/pages/TrackPage/Lyrics/Lyrics.tsx
@@ -13,8 +13,18 @@ function splitText(text) {
   return text.split(/\n{2,}|\n/);
 }
 
+function splitArrayInMiddle(arr: Array<string>) {
+  const middle = Math.floor(arr.length / 2);
+  const firstHalf = arr.slice(0, middle);
+  const secondHalf = arr.slice(middle);
+
+  return { firstHalf, secondHalf };
+}
+
 const Lyrics = ({ isrc }: Props): JSX.Element => {
   const [fetchedLyrics, setFetchedLyrics] = useState<any[]>(null);
+  const [columns, setColumns] = useState<'one' | 'two'>('one');
+  const [isFetching, setIsFetching] = useState(true);
 
   useEffect(() => {
     const fetchLyrics = async () => {
@@ -22,15 +32,40 @@ const Lyrics = ({ isrc }: Props): JSX.Element => {
         const result = (await getLyrics(isrc)) as LyricsType;
         const lyrics = splitText(result.lyrics_body);
         setFetchedLyrics(lyrics);
+        setIsFetching(false);
       } catch {
         setFetchedLyrics(['Lyrics not found.']);
+        setIsFetching(false);
       }
     };
     fetchLyrics();
   }, [isrc]);
 
   const DisplayLyrics = () => {
+    if (isFetching) return <div>Looking for lyrics.</div>;
+
     if (fetchedLyrics?.length > 1) {
+      if (columns === 'two') {
+        const result = splitArrayInMiddle(fetchedLyrics);
+        return (
+          <div style={{ display: 'flex' }}>
+            <div className={styles.lyrics} style={{ width: '50%' }}>
+              {result.firstHalf.map((lyric, index) => (
+                <Paragraph key={index} as="div">
+                  {lyric}
+                </Paragraph>
+              ))}
+            </div>
+            <div className={styles.lyrics} style={{ width: '50%' }}>
+              {result.secondHalf.map((lyric, index) => (
+                <Paragraph key={index} as="div">
+                  {lyric}
+                </Paragraph>
+              ))}
+            </div>
+          </div>
+        );
+      }
       return (
         <div className={styles.lyrics}>
           {fetchedLyrics.map((lyric, index) => (
@@ -52,6 +87,10 @@ const Lyrics = ({ isrc }: Props): JSX.Element => {
       <Heading className={styles.lyrics__heading} as="h3">
         Lyrics
       </Heading>
+      <div className={styles.toolbar}>
+        <button onClick={() => setColumns('one')}>Single column</button>
+        <button onClick={() => setColumns('two')}>Two columns</button>
+      </div>
       <DisplayLyrics />
     </div>
   );
