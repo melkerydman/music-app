88a3188422edce7ff65fd367c4eb2427de857c8d
melkerydman <melkerrydman@gmail.com>
2023-01-31 08:55:57 +0100

    PageHeader layout - mainly image hiding, remove comments

--- components/layout/PageHeader/PageHeader.tsx
+++ components/layout/PageHeader/PageHeader.tsx
@@ -1,7 +1,8 @@
 import Image from 'next/future/image';
 import { Display, Heading } from '../../Typography/Typography';
 import styles from './PageHeader.module.scss';
-import NewGrid from '../../NewGrid/NewGrid';
+// import NewGrid from '../../NewGrid/NewGrid';
+import { handleClassName } from '../../../utilities/helpers';
 
 interface Props {
   image: SpotifyApi.ImageObject;
@@ -14,30 +15,57 @@ const PageHeader = ({ image, heading, subHeading }: Props) => {
 
   return (
     <header className={styles.header}>
-      <NewGrid container>
-        <NewGrid item sm={3}>
-          <div className={styles['image-wrapper']}>
-            <Image
-              src={url}
-              alt={heading}
-              className={styles.image}
-              fill
-              sizes="(max-width: 768px) 100vw,
+      <div className={styles['image-wrapper']}>
+        <Image
+          src={url}
+          alt={heading}
+          className={styles.image}
+          fill
+          sizes="(max-width: 768px) 100vw,
               25vw"
-              priority
-            />
-          </div>
-        </NewGrid>
-        <NewGrid item sm={9} className={styles.titles}>
-          <Heading className={styles.titles__sub} as="h4">
-            {subHeading}
-          </Heading>
-          <Display className={styles.titles__main} small as="h1">
-            {heading}
-          </Display>
-        </NewGrid>
-      </NewGrid>
+          priority
+        />
+      </div>
+      <div className={styles.titles}>
+        <Heading
+          className={handleClassName([styles.titles__sub, 'h5'])}
+          as="h4"
+        >
+          {subHeading}
+        </Heading>
+        <Display className={styles.titles__main} small as="h1">
+          {heading}
+        </Display>
+      </div>
     </header>
+    // <header className={styles.header}>
+    //   <NewGrid container>
+    //     <NewGrid item sm={3}>
+    //       <div className={styles['image-wrapper']}>
+    //         <Image
+    //           src={url}
+    //           alt={heading}
+    //           className={styles.image}
+    //           fill
+    //           sizes="(max-width: 768px) 100vw,
+    //           25vw"
+    //           priority
+    //         />
+    //       </div>
+    //     </NewGrid>
+    //     <NewGrid item sm={9} className={styles.titles}>
+    //       <Heading
+    //         className={handleClassName([styles.titles__sub, 'h5'])}
+    //         as="h4"
+    //       >
+    //         {subHeading}
+    //       </Heading>
+    //       <Display className={styles.titles__main} small as="h1">
+    //         {heading}
+    //       </Display>
+    //     </NewGrid>
+    //   </NewGrid>
+    // </header>
   );
 };
 
