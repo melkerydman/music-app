4a0756c9c3658450dbf79c63951f666a5a19fa60
melkerydman <melkerrydman@gmail.com>
2023-01-31 22:05:39 +0100

    Attemped fix to slider on ios

--- components/InputSlider/InputSlider.tsx
+++ components/InputSlider/InputSlider.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
@@ -21,20 +21,30 @@ const InputSlider: React.FC<Props> = ({
   maxValue,
   step,
 }): JSX.Element => {
-  // const [value, setValue] = useState(0);
+  // Use state to track whether the thumb is being dragged
+  const [dragging, setDragging] = useState(false);
+
+  // Store the slider reference
   const sliderRef = useRef(null);
 
-  const handleSliderChange = (e) => {
-    const slider = sliderRef.current;
-    const sliderWidth = slider.offsetWidth;
-    const sliderX = slider.getBoundingClientRect().x;
-    const mouseX = e.clientX;
-    const range = maxValue - minValue;
-    const percentage = ((mouseX - sliderX) / sliderWidth) * 100;
-    const newValue = minValue + range * (percentage / 100);
+  // Store the previous touch position
+  const previousTouchX = useRef(0);
 
-    if (newValue >= minValue - 1 && newValue <= maxValue) {
-      setValue(Math.ceil(newValue));
+  // Handle changes to the slider value by mouse events
+  const handleSliderChange = (e) => {
+    // Only process events if we are not currently dragging the thumb
+    if (!dragging) {
+      const slider = sliderRef.current;
+      const sliderWidth = slider.offsetWidth;
+      const sliderX = slider.getBoundingClientRect().x;
+      const mouseX = e.clientX;
+      const range = maxValue - minValue;
+      const percentage = ((mouseX - sliderX) / sliderWidth) * 100;
+      const newValue = minValue + range * (percentage / 100);
+
+      if (newValue >= minValue - 1 && newValue <= maxValue) {
+        setValue(Math.ceil(newValue));
+      }
     }
   };
 
@@ -52,6 +62,30 @@ const InputSlider: React.FC<Props> = ({
     } else if (e.key === 'ArrowRight') {
       incrementValue();
     }
+  };
+
+  // Handle changes to the slider value by touch events
+  const handleSliderTouchMove = (e) => {
+    // Update the dragging flag
+    setDragging(true);
+
+    // Calculate the change in touch position
+    const touchX = e.touches[0].clientX;
+    const deltaX = touchX - previousTouchX.current;
+
+    // Update the previous touch position
+    previousTouchX.current = touchX;
+
+    // Adjust the value based on the change in touch position
+    const newValue = value + deltaX / 2;
+    if (newValue >= minValue - 1 && newValue <= maxValue) {
+      setValue(Math.ceil(newValue));
+    }
+  };
+
+  // Reset the touch event state when the user lifts their finger
+  const handleSliderTouchEnd = () => {
+    setDragging(false);
   };
 
   const trackTicks = [];
@@ -89,6 +123,11 @@ const InputSlider: React.FC<Props> = ({
             handleSliderChange(e);
           }
         }}
+        onTouchStart={(e) => {
+          previousTouchX.current = e.touches[0].clientX;
+        }}
+        onTouchMove={handleSliderTouchMove}
+        onTouchEnd={handleSliderTouchEnd}
       >
         <div className={styles.slider__track}>{trackTicks}</div>
         <div
@@ -114,3 +153,132 @@ const InputSlider: React.FC<Props> = ({
 };
 
 export default InputSlider;
+
+// import React, { useRef, useState } from 'react';
+
+// import AddIcon from '@mui/icons-material/Add';
+// import RemoveIcon from '@mui/icons-material/Remove';
+
+// import styles from './InputSlider.module.scss';
+// import { handleClassName } from '../../utilities/helpers';
+
+// type Props = {
+//   value: number;
+//   setValue: (number) => void;
+//   minValue: number;
+//   maxValue: number;
+//   step: number;
+// };
+
+// const InputSlider: React.FC<Props> = ({
+//   value,
+//   setValue,
+//   minValue,
+//   maxValue,
+//   step,
+// }): JSX.Element => {
+//   // Use state to track whether the thumb is being dragged
+//   const [dragging, setDragging] = useState(false);
+
+//   // Store the slider reference
+//   const sliderRef = useRef(null);
+
+//   // Store the previous touch position
+//   const previousTouchX = useRef(0);
+
+//   // Handle changes to the slider value by mouse events
+//   const handleSliderChange = (e) => {
+//     // Only process events if we are not currently dragging the thumb
+//     if (!dragging) {
+//       const slider = sliderRef.current;
+//       const sliderWidth = slider.offsetWidth;
+//       const sliderX = slider.getBoundingClientRect().x;
+//       const mouseX = e.clientX;
+//       const range = maxValue - minValue;
+//       const percentage = ((mouseX - sliderX) / sliderWidth) * 100;
+//       const newValue = minValue + range * (percentage / 100);
+
+//       if (newValue >= minValue - 1 && newValue <= maxValue) {
+//         setValue(Math.ceil(newValue));
+//       }
+//     }
+//   };
+
+//   // Decrement the value
+//   const decrementValue = () => {
+//     setValue(Math.max(minValue, value - 1));
+//   };
+
+//   // Increment the value
+//   const incrementValue = () => {
+//     setValue(Math.min(maxValue, value + 1));
+//   };
+
+//   // Handle changes to the slider value by touch events
+//   const handleSliderTouchMove = (e) => {
+//     // Update the dragging flag
+//     setDragging(true);
+
+//     // Calculate the change in touch position
+//     const touchX = e.touches[0].clientX;
+//     const deltaX = touchX - previousTouchX.current;
+
+//     // Update the previous touch position
+//     previousTouchX.current = touchX;
+
+//     // Adjust the value based on the change in touch position
+//     const newValue = value + deltaX / 2;
+//     if (newValue >= minValue - 1 && newValue <= maxValue) {
+//       setValue(Math.ceil(newValue));
+//     }
+//   };
+
+//   // Reset the touch event state when the user lifts their finger
+//   const handleSliderTouchEnd = () => {
+//     setDragging(false);
+//   };
+
+//   // Handle changes to the slider value by keyboard events
+//   const handleThumbKeyDown = (e) => {
+//     if (e.key === 'ArrowLeft') {
+//       decrementValue();
+//       e.preventDefault();
+//     } else if (e.key === 'ArrowRight') {
+//       incrementValue();
+//       e.preventDefault();
+//     }
+//   };
+
+//   return (
+//     <div
+//       className={handleClassName([styles.slider, dragging && styles.dragging])}
+//       ref={sliderRef}
+//       onMouseDown={handleSliderChange}
+//       onTouchStart={(e) => {
+//         previousTouchX.current = e.touches[0].clientX;
+//       }}
+//       onTouchMove={handleSliderTouchMove}
+//       onTouchEnd={handleSliderTouchEnd}
+//       tabIndex={0}
+//       onKeyDown={handleThumbKeyDown}
+//     >
+//       <div className={styles.thumb} />
+//       <div className={styles.track}>
+//         <div
+//           className={styles.fill}
+//           style={{
+//             width: `${((value - minValue) / (maxValue - minValue)) * 100}% `,
+//           }}
+//         />
+//       </div>
+//       <button type="button" onClick={decrementValue} className={styles.button}>
+//         <RemoveIcon />
+//       </button>
+//       <button type="button" onClick={incrementValue} className={styles.button}>
+//         <AddIcon />
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default InputSlider;
